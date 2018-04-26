6db6ed95f56f21e82360cba7dde1748f14467f37
Chi Chang <edingroot@gmail.com>
2018-04-26 15:20:10 +0800

    Import to repo.

---
+++ readme.md
@@ -0,0 +1,67 @@
+# HTTPP Standalone Demo
+
+This is a demo usage of daedric/httpp with standalone source code (tested on macOS High Sierra).  
+
+
+## Configuration Steps
+
+### Part A: Install commonpp
+Download and install [daedric/commonpp](https://github.com/daedric/commonpp)
+which is required by [daedric/HTTPP](https://github.com/daedric/httpp).
+
+Clone the git repository.  
+``` $ git clone https://github.com/daedric/commonpp ```
+
+Modify installation path from /usr/* to /usr/local/* ```CMakeLists.txt```:
+```
+ * CMAKE_INSTALL_PREFIX: /usr/local
+ * INSTALL_LIB_DIR: local/lib
+ * INSTALL_BIN_DIR: local/bin
+ * INSTALL_INCLUDE_DIR: local/include
+```
+
+Configure and build:  
+```
+$ make cmake
+$ make -j4
+$ make test
+``` 
+
+Build package, and ``` build/commonpp-0.1.1-Darwin.tar.gz ``` is generated:   
+```
+$ make package
+```
+
+Unarchive package files and copy to /usr/local/*:  
+```
+$ cd build/
+$ tar zxvf commonpp-0.1.1-Darwin.tar.gz 
+$ cd commonpp-0.1.1-Darwin
+$ sudo cp local/lib/*.a /usr/local/lib
+$ sudo cp -R local/lib/CMake/commonpp /usr/local/lib/CMake/
+$ sudo cp -R local/include/commonpp /usr/local/include/
+```
+
+### Part B: Install HTTPP
+Clone the git repository.  
+``` $ git clone https://github.com/daedric/httpp ```
+
+Modify installation path from /usr/* to /usr/local/* ```CMakeLists.txt```:
+```
+ * _CMAKE_DIR "local/lib/CMake/HTTPP"
+ * INSTALL_LIB_DIR: local/lib
+ * INSTALL_BIN_DIR: local/bin
+ * INSTALL_INCLUDE_DIR: local/include
+ * OPTION(BUILD_TESTS "Build the tests" OFF)
+ * OPTION(BUILD_EXAMPLES "Build the examples" ON)
+ * OPTION(BUILD_SHARED_LIBS "Build shared lib instead of static ones" OFF)
+```  
+
+Configure and build:  
+```
+$ mkdir build
+$ cmake ..
+$ make -j4
+$ make test
+* sudo make install
+``` 
---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+/*
+ * Part of HTTPP.
+ *
+ * Distributed under the 2-clause BSD licence (See LICENCE.TXT file at the
+ * project root).
+ *
+ * Copyright (c) 2014 Thomas Sanchez.  All rights reserved.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <chrono>
+
+#include <httpp/HttpServer.hpp>
+#include <httpp/http/Utils.hpp>
+#include <httpp/utils/Exception.hpp>
+
+using HTTPP::HttpServer;
+using HTTPP::HTTP::Request;
+using HTTPP::HTTP::Connection;
+using HTTPP::HTTP::HttpCode;
+
+void handler(Connection* connection)
+{
+    read_whole_request(connection, [](std::unique_ptr<
+                                          HTTPP::HTTP::helper::ReadWholeRequest> hndl,
+                                      const boost::system::error_code& ec) {
+        const auto& body = hndl->body;
+        const auto& connection = hndl->connection;
+        const auto& request  = connection->request();
+
+        if (ec)
+        {
+            throw HTTPP::UTILS::convert_boost_ec_to_std_ec(ec);
+        }
+
+        std::ostringstream out;
+        out << request;
+
+        if (body.empty())
+        {
+            connection->response()
+                .setCode(HttpCode::Ok)
+                .setBody("request received: " + out.str());
+        }
+        else
+        {
+            unsigned long bodySize = body.size();
+            std::string bodyContent(body.begin(), body.end());
+
+            connection->response()
+                .setCode(HttpCode::Ok)
+                .setBody("request received entirely: " + out.str() +
+                         ", body size: " + std::to_string(body.size()) +
+                         "\n\nBody:\n" + bodyContent);
+        }
+
+        HTTPP::HTTP::setShouldConnectionBeClosed(request, connection->response());
+        connection->sendResponse(); // connection pointer may become invalid
+
+        auto end = Request::Clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(
+            end - request.received);
+        std::cout << "Request handled in: " << elapsed.count() << "us"
+                  << std::endl;
+    });
+}
+
+int main(int, char**)
+{
+    HttpServer server;
+    server.start();
+    server.setSink(&handler);
+    server.bind("0.0.0.0", "8080");
+    server.bind("localhost", "8081");
+    server.bind("localhost", "8082");
+    while (true) std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
